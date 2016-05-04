f6e51346891fd8bb74433e32fecc8c3ff27bfc2e
Jorge Chavez <echavez@nearsoft.com>
2016-05-03 19:24:46 -0600

    Change common vars names

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 // Portal NS configuration
 
-var host = process.env.HOST;
-var token = process.env.TOKEN || "1B724F94C3EDC1DA6FD7294D1C611";
+var host = process.env.PORTAL_NS_HOST;
+var token = process.env.PORTAL_NS_TOKEN || "1B724F94C3EDC1DA6FD7294D1C611";
 
 var config = {
   host: host,
