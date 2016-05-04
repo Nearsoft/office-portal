d849340a02fa5cc014e315df266a7d81579c8b6b
Jorge Chavez <echavez@nearsoft.com>
2016-05-03 19:11:33 -0600

    Add optional token for the test process

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 // Portal NS configuration
 
 var host = process.env.HOST;
-var token = process.env.TOKEN;
+var token = process.env.TOKEN || "1B724F94C3EDC1DA6FD7294D1C611";
 
 var config = {
   host: host,
